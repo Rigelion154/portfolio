233b00aec62e0eb2b11fc6368f3a1efce2354d34
Rigelion <cupofbeer123@gmail.com>
2023-06-30 17:08:56 +0300

    fix: fix greeting animation for mobile

--- src/js/components/greetings/greeting.js
+++ src/js/components/greetings/greeting.js
@@ -5,18 +5,18 @@ import {nav} from "../header/header";
 
 export function greetings() {
   let tl = gsap.timeline()
-  tl.from('.greeting span', {y: 120, opacity: 0, ease: 'back', duration: 1.7, stagger: 0.1})
+  tl.from('.greeting span', {yPercent: 120, opacity: 0, ease: 'back', duration: 1.7, stagger: 0.1})
     .fromTo('.o', {rotation: 0, duration: 1, ease: 'bounce'}, {rotation: 20, duration: .7})
     .to('.o', {rotation: 180, duration: .7, ease: 'bounce'})
-    .to('.h', {y: 100, rotation: 180, duration: .7, ease: 'bounce'})
-    .to('.e', {y: 113, ease: 'bounce', duration: 0.3})
-    .to('.l', {y: 113, ease: 'bounce', duration: 0.5})
-    .to('.ll', {y: 113, ease: 'bounce', duration: 0.4})
-    .to('.o', {y: 0, duration: .7, ease: 'bounce'})
+    .to('.h', {yPercent: 200, rotation: 360, duration: .7, ease: 'bounce'})
+    .to('.e', {yPercent: 200, ease: 'bounce', duration: 0.3})
+    .to('.l', {yPercent: 200, ease: 'bounce', duration: 0.5})
+    .to('.ll', {yPercent: 200, ease: 'bounce', duration: 0.4})
+    .to('.o', {yPercent: 110, duration: .7, ease: 'bounce'})
     .to('.greeting', {opacity: 0, duration: 1})
-    .from('.wrapper', {opacity: 0, duration: 1, onComplete: () => {
-        document.body.style.overflow = 'auto'
-      }})
+    // .from('.wrapper', {opacity: 0, duration: 1, onComplete: () => {
+    //     document.body.style.overflow = 'auto'
+    //   }})
   return tl
 }
 
