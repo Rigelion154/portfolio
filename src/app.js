cf0bcca86b90d4f223410977c2a4af88dddad876
Rigelion <cupofbeer123@gmail.com>
2023-07-04 21:31:20 +0300

    fix: fixed start page position on reload

--- src/app.js
+++ src/app.js
@@ -4,16 +4,11 @@ import {nav} from "./js/components/header/header";
 import {gsap} from "gsap";
 import {scrollAnimation} from "./js/utils/scrollAnimation";
 
-window.addEventListener('DOMContentLoaded', function() {
-  let render = gsap.timeline()
-  render.add(greetings())
-});
-
 function app() {
   let render = gsap.timeline()
   render.add(greetings())
   render.add(nav)
-  render.add(scrollAnimation())
+  // render.add(scrollAnimation())
 }
 
 app()
--- src/js/components/greetings/greeting.js
+++ src/js/components/greetings/greeting.js
@@ -1,22 +1,27 @@
 import '../../../styles/components/greeting.css'
 
 import {gsap} from "gsap";
+import {scrollAnimation} from "../../utils/scrollAnimation";
 
 export function greetings() {
-  let tl = gsap.timeline()
-  tl.from('.greeting span', {yPercent: 120, opacity: 0, ease: 'back', duration: 1.7, stagger: 0.1})
-    .fromTo('.o', {rotation: 0, duration: 1, ease: 'bounce'}, {rotation: 20, duration: .7})
-    .to('.o', {rotation: 180, duration: .7, ease: 'bounce'})
-    .to('.h', {yPercent: 200, rotation: 360, duration: .7, ease: 'bounce'})
-    .to('.e', {yPercent: 200, ease: 'bounce', duration: 0.3})
-    .to('.l', {yPercent: 200, ease: 'bounce', duration: 0.5})
-    .to('.ll', {yPercent: 200, ease: 'bounce', duration: 0.4})
-    .to('.o', {yPercent: 110, duration: .7, ease: 'bounce'})
-    .to('.greeting', {opacity: 0, duration: 1})
-    .from('.wrapper', {opacity: 0, duration: 1, onComplete: () => {
-        document.body.style.overflow = 'auto'
-      }})
-  return tl
+    let tl = gsap.timeline()
+    tl.from('.greeting span', {yPercent: 120, opacity: 0, ease: 'back', duration: 1.7, stagger: 0.1})
+        .fromTo('.o', {rotation: 0, duration: 1, ease: 'bounce'}, {rotation: 20, duration: .7})
+        .to('.o', {rotation: 180, duration: .7, ease: 'bounce'})
+        .to('.h', {yPercent: 200, rotation: 360, duration: .7, ease: 'bounce'})
+        .to('.e', {yPercent: 200, ease: 'bounce', duration: 0.3})
+        .to('.l', {yPercent: 200, ease: 'bounce', duration: 0.5})
+        .to('.ll', {yPercent: 200, ease: 'bounce', duration: 0.4})
+        .to('.o', {yPercent: 110, duration: .7, ease: 'bounce'})
+        .to('.greeting', {opacity: 0, duration: 1})
+        .from('.wrapper', {
+            opacity: 0, duration: 1, onComplete: () => {
+                document.body.style.overflow = 'auto'
+                document.querySelector('.main').style.display = 'block'
+                scrollAnimation()
+            }
+        })
+    return tl
 }
 
 
